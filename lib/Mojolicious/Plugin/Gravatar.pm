e8eec65b0e78d281acef59dffc0421bf9ed5c9cf
Ben Meyer <thebenmeyer@gmail.com>
2016-07-13 21:50:47 +0000

    updated Gravatar.pm to match https://github.com/KeystoneIT/Ama/blob/Dev/lib/Mojolicious/Plugin/Gravatar.pm

--- lib/Mojolicious/Plugin/Gravatar.pm
+++ lib/Mojolicious/Plugin/Gravatar.pm
@@ -1,32 +1,39 @@
 package Mojolicious::Plugin::Gravatar;
 use Mojo::Base 'Mojolicious::Plugin';
-
+use Gravatar::URL;
 our $VERSION = '0.01';
 
 sub register {
   my ($self, $app) = @_;
+  $app->helper(
+    'gravatar.url' => sub {
+      my ($self, $args) = @_;
+      return gravatar_url( email => $args->{email} , rating => $args->{rating});
+    }
+  );
 }
 
 1;
-__END__
+
+__END__                     
 
 =encoding utf8
 
 =head1 NAME
 
-Mojolicious::Plugin::Gravatar - Mojolicious Plugin
+Mojolicious::Plugin::Gravatar - Mojolicious Plugin for creating a gravatar url
 
 =head1 SYNOPSIS
 
   # Mojolicious
-  $self->plugin('Gravatar');
+  $self->plugin('gravatar');
 
   # Mojolicious::Lite
-  plugin 'Gravatar';
+  plugin 'gravatar';
 
 =head1 DESCRIPTION
 
-L<Mojolicious::Plugin::Gravatar> is a L<Mojolicious> plugin.
+L<Mojolicious::Plugin::Gravatar> is a L<Mojolicious> plugin for Mojolicious Apps to create a Gravatar URL with an email address using Gravatar::URL
 
 =head1 METHODS
 
